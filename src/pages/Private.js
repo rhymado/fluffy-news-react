426c60e139aef4a27c96bc06afbb39b637603ebb
rhymaado <mightydhiyamatra@yahoo.co.id>
2022-11-01 16:57:16 +0700

    test redirect class

--- src/pages/Private.js
+++ src/pages/Private.js
@@ -1,13 +1,25 @@
-import React, { useEffect } from "react";
-import { useNavigate } from "react-router-dom";
+// import React, { useEffect } from "react";
+// import { useNavigate } from "react-router-dom";
 
-function Private() {
-  const navigate = useNavigate();
-  useEffect(() => {
+// function Private() {
+//   const navigate = useNavigate();
+//   useEffect(() => {
+//     const isLogin = localStorage.getItem("isLogin");
+//     if (isLogin !== "true") navigate("/register");
+//   }, [navigate]);
+//   return <div>Private</div>;
+// }
+import React, { Component } from "react";
+import withNavigate from "../helpers/withNavigate";
+
+class Private extends Component {
+  componentDidMount() {
     const isLogin = localStorage.getItem("isLogin");
-    if (isLogin !== "true") navigate("/register");
-  }, [navigate]);
-  return <div>Private</div>;
+    if (!isLogin) this.props.navigate("/register");
+  }
+  render() {
+    return <div>Private</div>;
+  }
 }
 
-export default Private;
+export default withNavigate(Private);
